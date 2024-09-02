34ea91b79dcf57fc42b264daea930b69824f4a37
Per Johansson <mail@perjohansson.net>
2024-09-02 21:56:05 +0200

    fixes

--- src/ref_con.rs
+++ src/ref_con.rs
@@ -1,14 +1,25 @@
-pub type TrampolineRefcon = *mut Box<dyn FnOnce()>;
-pub type TrampolineCallback = extern "C" fn(TrampolineRefcon);
+use std::{ffi::c_void, ptr};
+
+#[repr(transparent)]
+pub struct TrampolineRefcon(*mut c_void);
+
+#[repr(transparent)]
+pub struct TrampolineCallback(extern "C" fn(TrampolineRefcon));
 
 pub fn create_trampoline<F: FnOnce() + 'static>(
     wrapped_closure: F,
 ) -> (TrampolineCallback, TrampolineRefcon) {
-    pub extern "C" fn caller(closure_ptr: TrampolineRefcon) {
+    pub extern "C" fn caller<F>(closure_ptr: TrampolineRefcon)
+    where
+        F: FnOnce(),
+    {
         unsafe {
-            let closure = Box::from_raw(closure_ptr);
+            let closure: F = ptr::read(closure_ptr.0.cast());
             closure();
         };
     }
-    (caller, Box::into_raw(Box::new(Box::new(wrapped_closure))))
+    (
+        TrampolineCallback(caller::<F>),
+        TrampolineRefcon(Box::into_raw(Box::new(wrapped_closure)) as *mut c_void),
+    )
 }

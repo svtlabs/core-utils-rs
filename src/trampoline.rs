1f048b8a76842e463fcbe39db454866bcce7cfee
Per Johansson <mail@perjohansson.net>
2024-09-17 22:28:55 +0200

    fix trampoline

--- src/trampoline.rs
+++ src/trampoline.rs
@@ -4,13 +4,17 @@ use std::{ffi::c_void, ptr};
 pub struct TrampolineRefcon(*mut c_void);
 
 #[repr(transparent)]
-pub struct TrampolineCallback<Ret = (), Param = TrampolineRefcon>(
+pub struct TrampolineLeftCallback<Ret = (), Param = TrampolineRefcon>(
     extern "C" fn(TrampolineRefcon, Param) -> Ret,
 );
+#[repr(transparent)]
+pub struct TrampolineRightCallback<Ret = (), Param = TrampolineRefcon>(
+    extern "C" fn(Param, TrampolineRefcon) -> Ret,
+);
 
-pub fn create_trampoline<Ret, Param, F: FnOnce(Param) -> Ret + 'static>(
+pub fn create_left_trampoline<Ret, Param, F: FnOnce(Param) -> Ret + 'static>(
     wrapped_closure: F,
-) -> (TrampolineCallback<Ret, Param>, TrampolineRefcon) {
+) -> (TrampolineLeftCallback<Ret, Param>, TrampolineRefcon) {
     pub extern "C" fn caller<Ret, Param, F>(closure_ptr: TrampolineRefcon, param: Param) -> Ret
     where
         F: FnOnce(Param) -> Ret,
@@ -22,7 +26,26 @@ pub fn create_trampoline<Ret, Param, F: FnOnce(Param) -> Ret + 'static>(
         }
     }
     (
-        TrampolineCallback(caller::<Ret, Param, F>),
+        TrampolineLeftCallback(caller::<Ret, Param, F>),
+        TrampolineRefcon(Box::into_raw(Box::new(wrapped_closure)).cast()),
+    )
+}
+
+pub fn create_right_trampoline<Ret, Param, F: FnOnce(Param) -> Ret + 'static>(
+    wrapped_closure: F,
+) -> (TrampolineRightCallback<Ret, Param>, TrampolineRefcon) {
+    pub extern "C" fn caller<Ret, Param, F>(param: Param, closure_ptr: TrampolineRefcon) -> Ret
+    where
+        F: FnOnce(Param) -> Ret,
+    {
+        println!("caller");
+        unsafe {
+            let closure: F = ptr::read(closure_ptr.0.cast());
+            closure(param)
+        }
+    }
+    (
+        TrampolineRightCallback(caller::<Ret, Param, F>),
         TrampolineRefcon(Box::into_raw(Box::new(wrapped_closure)).cast()),
     )
 }
